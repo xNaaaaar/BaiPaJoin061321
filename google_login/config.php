dc4a707f26991a023b3bd92861314cd27f923b6f
Mo0nRiv3r <69462418+Mo0nRiv3r@users.noreply.github.com>
2021-09-17 17:20:34 +0800

    added: resources for google login

---
+++ google_login/config.php
@@ -0,0 +1,14 @@
+<?php
+
+	require_once 'vendor/autoload.php';
+
+	$google_client = new Google_Client();
+
+	$google_client -> setClientId('1031903496080-kk99fgt2bf3tgc68eva47cao4lnnm4rg.apps.googleusercontent.com');
+	$google_client -> setClientSecret('cDLDjuBFoaAzJ3yTR5Ej7MUR');
+	$google_client -> setRedirectUri('http://localhost/BaiPaJoin42/index.php');
+	$google_client -> addScope('email');
+	$google_client -> addScope('profile');
+	$google_client -> addScope('openid');
+
+?>
