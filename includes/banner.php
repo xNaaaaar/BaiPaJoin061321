cadab73cfc96715ef45739f86f774c4642eeb005
Mo0nRiv3r <69462418+Mo0nRiv3r@users.noreply.github.com>
2021-10-01 13:50:13 +0800

    refactor: SQL Query and disable feature for organizers

--- includes/banner.php
+++ includes/banner.php
@@ -10,14 +10,18 @@
 				<?php
 					if(isset($_SESSION['organizer']))
 						echo "<a href='adventures_posted.php' class='wow fadeInLeft' data-wow-duration='1s' data-wow-delay='.6s'>Search Now &#187;</a>";
-					else
+					else if(!isset($_SESSION['organizer'])) {
+						$current_date = date('Y-m-d');
 						echo "<a href='islands.php' class='wow fadeInLeft' data-wow-duration='1s' data-wow-delay='.6s'>Search Now &#187;</a>";
-						$adv_db = DB::query("SELECT adv_id FROM adventure WHERE adv_status != 'full'", array(), "READ");
+						$adv_db = DB::query("SELECT adv_id FROM adventure WHERE adv_date > '$current_date' AND adv_status != 'full'", array(), "READ");
 						$adv_list = array();
-						foreach($adv_db as $item)
+						foreach($adv_db as $item) {
 							array_push($adv_list, $item[0]);
+							file_put_contents('debug.log', date('h:i:sa').' => '. $item[0] . "\n" . "\n", FILE_APPEND);
+						}
 						$random_adv = array_rand($adv_list,1);						
 						echo "<a href='place.php?id=".$adv_list[$random_adv]."' class='wow fadeInLeft' data-wow-duration='1.5s' data-wow-delay='1s'>Surprise Me &#187;</a>";
+					}						
 				?>
 
 			</div>
