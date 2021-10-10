db877e01a628bfe44d2abeda85704697f39fa4c3
Melnar <narancit@gmail.com>
2021-10-10 11:26:21 +0800

    Update reports_booking-cancel.php

--- reports_booking-cancel.php
+++ reports_booking-cancel.php
@@ -77,49 +77,35 @@
 				</form>
 			</main>
 
-			<?php
-<<<<<<< Updated upstream
-				if(isset($_POST['btnRequest'])){
-					$txtReason = ucfirst(trim($_POST['txtReason']));
-					##
-					$booked_db = DB::query("SELECT * FROM booking WHERE book_id=?", array($_GET['book_id']), "READ");
-					$booked = $booked_db[0];
-					##
-					DB::query("INSERT INTO request (req_user, req_type, req_dateprocess, req_amount, req_status, req_reason, req_rcvd, book_id) VALUES(?,?,?,?,?,?,?,?)", array('joiner', 'cancel', date("Y-m-d"), $booked['book_totalcosts'], 'pending', $txtReason, 0, $_GET['book_id']), "CREATE");
-=======
-			## REQUEST CANCEL FOR JOINER
-			if(isset($_POST['btnRequest-Joiner'])){
-				$txtReason = ucfirst(trim($_POST['txtReason']));
-				##
-				$booked_db = DB::query("SELECT * FROM booking WHERE book_id=?", array($_GET['book_id']), "READ");
-				$booked = $booked_db[0];
-				##
-				DB::query("INSERT INTO request (req_user, req_type, req_dateprocess, req_amount, req_status, req_reason, req_rcvd, book_id, adv_id) VALUES(?,?,?,?,?,?,?,?,?)", array('joiner', 'cancel', date("Y-m-d"), $booked['book_totalcosts'], 'pending', $txtReason, 0, $_GET['book_id'], NULL), "CREATE");
->>>>>>> Stashed changes
+		<?php
+		## REQUEST CANCEL FOR JOINER
+		if(isset($_POST['btnRequest-Joiner'])){
+			$txtReason = ucfirst(trim($_POST['txtReason']));
+			##
+			$booked_db = DB::query("SELECT * FROM booking WHERE book_id=?", array($_GET['book_id']), "READ");
+			$booked = $booked_db[0];
+			##
+			DB::query("INSERT INTO request (req_user, req_type, req_dateprocess, req_amount, req_status, req_reason, req_rcvd, book_id, adv_id) VALUES(?,?,?,?,?,?,?,?,?)", array('joiner', 'cancel', date("Y-m-d"), $booked['book_totalcosts'], 'pending', $txtReason, 0, $_GET['book_id'], NULL), "CREATE");
 
-					## EMAIL + SMS NOTIFICATION
-					$joiner_db = DB::query("SELECT * FROM joiner WHERE joiner_id = ?", array($_SESSION['joiner']), "READ");
-					$joiner_info = $joiner_db[0];
+				## EMAIL + SMS NOTIFICATION
+				$joiner_db = DB::query("SELECT * FROM joiner WHERE joiner_id = ?", array($_SESSION['joiner']), "READ");
+				$joiner_info = $joiner_db[0];
 
-					$sms_sendto = $joiner_info['joiner_phone'];
-					$sms_message = "Hi ".$joiner_info['joiner_fname']."! Your request for cancellation has been submitted on ".date('d-M-Y').".";
+				$sms_sendto = $joiner_info['joiner_phone'];
+				$sms_message = "Hi ".$joiner_info['joiner_fname']."! Your request for cancellation has been submitted on ".date('d-M-Y').".";
 
-					send_sms($sms_sendto,$sms_message);
+				send_sms($sms_sendto,$sms_message);
 
-					$email_message = html_cancellation_message($joiner_info['joiner_fname'], 'Joiner');
+				$email_message = html_cancellation_message($joiner_info['joiner_fname'], 'Joiner');
 
-<<<<<<< Updated upstream
-					$img_address = array();
-					$img_name = array();
+				$img_address = array();
+				$img_name = array();
 
-					array_push($img_address,'images/cancel-bg.jpg','images/main-logo-green.png','images/cancel-img.jpg');
-					array_push($img_name,'background','logo','main');
+				array_push($img_address,'images/cancel-bg.jpg','images/main-logo-green.png','images/cancel-img.jpg');
+				array_push($img_name,'background','logo','main');
 
-					send_email($joiner_info['joiner_email'], "BOOKING CANCELLATION", $email_message, $img_address, $img_name);
+				send_email($joiner_info['joiner_email'], "BOOKING CANCELLATION", $email_message, $img_address, $img_name);
 
-					header("Location: request.php?cancel_success");
-				}
-=======
 				header("Location: request.php?cancel_success");
 			}
 
@@ -134,7 +120,6 @@
 				##
 				header("Location: request.php?cancel_success");
 			}
->>>>>>> Stashed changes
 			?>
 		</div>
 
